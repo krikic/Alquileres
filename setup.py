c71f83f5e8c8368c34658b9a6657393ff36ac010
krikic <david.curso.oracle@gmail.com>
2014-06-13 18:47:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author=['David Mádico'],
     author_email=['david.curso.oracle@gmail.com'],
     license='LICENSE.txt',
-    url = 'https://github.com/emili/EmilioRiosExamen.git',
+    url = 'https://github.com/krikic/Alquileres.git',
     download_url = 'https://github.com/emilioriosvz/EmilioRiosExamen/archive/master.zip',
     description='Alquiler de coches.',
     long_description=open('README.md').read()
